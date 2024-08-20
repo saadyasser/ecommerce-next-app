4f0c86b936816bbc088f729dee17797322ddab66
saadyasser <saad.wadi.229@gmail.com>
2024-08-20 12:25:46 +0300

    fix products list tests

--- src/components/ProductList/__test__/ProductList.test.tsx
+++ src/components/ProductList/__test__/ProductList.test.tsx
@@ -2,20 +2,19 @@ import { CART, PRODUCTS } from "@/contants";
 import ProductsList from "..";
 import { fireEvent, render, screen } from "@testing-library/react";
 
-describe.skip("Products Section", () => {
+describe("Products Section", () => {
   const addToCart = jest.fn();
   const decrementQuantity = jest.fn();
-  render(
-    <ProductsList
-      products={PRODUCTS}
-      cart={CART}
-      addToCart={addToCart}
-      decrementQuantity={decrementQuantity}
-    />
-  );
 
   test("Check The Products Container and Product List Are Found", () => {
-    screen.debug();
+    render(
+      <ProductsList
+        products={PRODUCTS}
+        cart={CART}
+        addToCart={addToCart}
+        decrementQuantity={decrementQuantity}
+      />
+    );
     const productsContainer = screen.getByRole("products-container");
 
     const productsList = screen.getAllByRole("product");
@@ -24,6 +23,15 @@ describe.skip("Products Section", () => {
   });
 
   test("Check If The Items Are Added To The Cart Successfully", () => {
+    render(
+      <ProductsList
+        products={PRODUCTS}
+        cart={CART}
+        addToCart={addToCart}
+        decrementQuantity={decrementQuantity}
+      />
+    );
+    screen.debug();
     const addToCartButtons = screen.getAllByText(/Add to Cart/);
     expect(addToCartButtons).toHaveLength(7);
     const firstButton = addToCartButtons[0];

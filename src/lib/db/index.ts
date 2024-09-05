6435fdea306567328ac34f25986d95d17dfe7743
saadyasser <saad.wadi.229@gmail.com>
2024-09-05 11:59:33 +0300

    test

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -2,6 +2,9 @@ import { Pool } from 'pg';
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false, // This will allow SSL connections without certificate validation
+    },
 });
 
 export default pool;
